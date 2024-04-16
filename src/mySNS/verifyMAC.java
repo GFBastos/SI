8fbd1e68fa9e6c4258a237e987be776b45ff6dff
GFBastos <130056462+GFBastos@users.noreply.github.com>
2024-04-16 14:09:39 +0100

    MAC classes

---
+++ src/mySNS/verifyMAC.java
@@ -0,0 +1,65 @@
+package mySNS;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+public class verifyMAC {
+	public Boolean init(String pass) throws IOException, NoSuchAlgorithmException, InvalidKeyException {
+		try {
+			FileInputStream fis = new FileInputStream("users.txt");
+			FileInputStream fisMAC = new FileInputStream("users.mac");
+			
+			byte[] password = pass.getBytes();
+			SecretKey key = new SecretKeySpec(password, "HmacSHA256");
+			
+			Mac m = Mac.getInstance("HmacSHA256");
+			m.init(key);
+			
+			byte[] b = new byte[16];
+			int i = fis.read(b);
+			
+			while(i != -1) {
+				m.update(b, 0, i);
+				i = fis.read(b);
+			}
+			
+			byte[] mac = m.doFinal();
+			fis.close();
+			
+			// Tranformar bytes em String para ser mais facil de comprar 
+			String sMAC = Base64.getEncoder().encodeToString(mac);
+			
+			byte[] macToBeVerified = new byte[fisMAC.available()];
+			fisMAC.read(macToBeVerified);
+			
+			String b_macToBeVerified = Base64.getEncoder().encodeToString(macToBeVerified);
+			
+			if(sMAC.equals(b_macToBeVerified)) {
+				System.out.println("Ok");
+				return true;
+			}
+			
+			System.out.println("Nok");
+			return false;
+			
+		}catch(IOException e) {
+			System.err.println(e.getMessage());
+			throw e;
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw e;
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw e;
+		}
+	}
+}
---
+++ src/mySNS/writeMAC.java
@@ -0,0 +1,48 @@
+package mySNS;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+public class writeMAC {
+	public void init(String pass) {
+		try {
+			FileInputStream fis = new FileInputStream("users.txt");
+			FileOutputStream fos = new FileOutputStream("users.mac");
+			
+			byte[] password = pass.getBytes();
+			SecretKey key = new SecretKeySpec(password, "HmacSHA256");
+			
+			Mac m = Mac.getInstance("HmacSHA256");
+			m.init(key);
+			
+			byte[] b = new byte[16];
+			int i = fis.read(b);
+			
+			while(i != -1) {
+				m.update(b, 0, i);
+				i = fis.read(b);
+			}
+			
+			byte[] mac = m.doFinal();
+			
+			fos.write(mac);
+			fos.close();
+			fis.close();
+		}catch(IOException e) {
+			System.err.println(e.getMessage());
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
